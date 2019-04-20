94265223578d1c1de68b14b566ad2e5131af8a27
Kamil Żarski <kmzarski@gmail.com>
2019-04-21 01:01:10 +0200

    prepare test class, create simple test method for newReader and NewsLoader

---
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -0,0 +1,51 @@
+package edu.iis.mto.staticmock;
+
+import edu.iis.mto.staticmock.reader.NewsReader;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.*;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class})
+public class NewsLoaderTest {
+//ConfigurationLoader  NewsReaderFactory
+
+    private NewsLoader newsLoader;
+    private ConfigurationLoader configurationLoader;
+    private NewsReader newsReader;
+    private Configuration configuration;
+    private IncomingNews incomingNews;
+
+    @Before
+    public void setUp() throws Exception {
+        newsLoader = new NewsLoader();
+        configuration = new Configuration();
+        incomingNews = new IncomingNews();
+
+        mockStatic(NewsReaderFactory.class);
+        mockStatic(ConfigurationLoader.class);
+
+        configurationLoader = mock(ConfigurationLoader.class);
+        newsReader = mock(NewsReader.class);
+        when(ConfigurationLoader.getInstance()).thenReturn(configurationLoader);
+        when(ConfigurationLoader.getInstance().loadConfiguration()).thenReturn(configuration);
+        Whitebox.setInternalState(configuration, "readerType", "readerType");
+        when(NewsReaderFactory.getReader("readerType")).thenReturn(newsReader);
+        when(newsReader.read()).thenReturn(incomingNews);
+    }
+
+    @Test
+    public void testForNewsReaderAfterNewsLoader() {
+        newsLoader.loadNews();
+        verify(newsReader,times(1)).read();
+    }
+
+
+}
