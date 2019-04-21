fbdd6132d80d9b0cb28d1d1d56103b70d44b85c6
pawo97 <pa-wo97@o2.pl>
2019-04-21 18:32:55 +0200

    Test2 - isReturnedTwoPublicNews

--- src/test/java/lab3_2/ConfigurationLoaderTest1.java
+++ src/test/java/lab3_2/ConfigurationLoaderTest1.java
@@ -1,10 +1,16 @@
 package lab3_2;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -19,11 +25,12 @@ import edu.iis.mto.staticmock.IncomingInfo;
 import edu.iis.mto.staticmock.IncomingNews;
 import edu.iis.mto.staticmock.NewsLoader;
 import edu.iis.mto.staticmock.NewsReaderFactory;
+import edu.iis.mto.staticmock.PublishableNews;
 import edu.iis.mto.staticmock.SubsciptionType;
 import edu.iis.mto.staticmock.reader.NewsReader;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class})
+@PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class, PublishableNews.class})
 
 public class ConfigurationLoaderTest1 {
 
@@ -61,11 +68,46 @@ public class ConfigurationLoaderTest1 {
         incomingNews.add(new IncomingInfo("NewNewsA", SubsciptionType.A));
         incomingNews.add(new IncomingInfo("NewNewsB", SubsciptionType.B));
         incomingNews.add(new IncomingInfo("NewNewsC", SubsciptionType.C));
-        incomingNews.add(new IncomingInfo("newsNONE", SubsciptionType.NONE));
+        incomingNews.add(new IncomingInfo("NewsNONE", SubsciptionType.NONE));
 
         newsLoader.loadNews();
 
         verify(configurationLoader, times(1)).loadConfiguration();
+    }
+
+    class PublishableNewsTest extends PublishableNews {
+
+        public List<String> publicContent = new ArrayList<>();
+        public List<String> subscribentContent = new ArrayList<>();
+
+        @Override
+        public void addPublicInfo(String content) {
+            this.publicContent.add(content);
+        }
+
+        @Override
+        public void addForSubscription(String content, SubsciptionType subscriptionType) {
+            this.subscribentContent.add(content);
+        }
+    }
+
+    @Test
+    public void isReturnTwoPublicNews() {
+        incomingNews.add(new IncomingInfo("NewNewsA", SubsciptionType.A));
+        incomingNews.add(new IncomingInfo("NewNewsB", SubsciptionType.B));
+        incomingNews.add(new IncomingInfo("NewNewsC", SubsciptionType.C));
+        incomingNews.add(new IncomingInfo("NewNewsNONE1", SubsciptionType.NONE));
+        incomingNews.add(new IncomingInfo("NewNewsNONE2", SubsciptionType.NONE));
+
+        mockStatic(PublishableNews.class);
+
+        when(PublishableNews.create()).thenReturn(new PublishableNewsTest());
+
+        PublishableNewsTest pubNews = (PublishableNewsTest) newsLoader.loadNews();
+
+        assertThat(pubNews.publicContent.get(0), is(equalTo("NewNewsNONE1")));
+        assertThat(pubNews.publicContent.get(1), is(equalTo("NewNewsNONE2")));
+        assertThat(pubNews.publicContent.size(), is(equalTo(2)));
     }
 
 }
