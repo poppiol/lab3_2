1e68ddfaedf12b82ad610e881634ceb84a52d60f
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-04-27 01:50:33 +0200

    oneCallLoadNewsShouldReadOnce() test added

---
+++ src/main/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -0,0 +1,48 @@
+package edu.iis.mto.staticmock;
+
+import edu.iis.mto.staticmock.reader.FileNewsReader;
+import edu.iis.mto.staticmock.reader.NewsReader;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+import static org.powermock.api.mockito.PowerMockito.*;
+
+@RunWith(PowerMockRunner.class) @PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class}) public class NewsLoaderTest {
+
+    private ConfigurationLoader configurationLoader;
+    private Configuration configuration;
+    private NewsReader newsReader;
+    private IncomingNews incomingNews;
+
+    @Before public void init() {
+        configuration = new Configuration();
+        newsReader = new FileNewsReader();
+        incomingNews = new IncomingNews();
+
+        mockStatic(ConfigurationLoader.class);
+        mockStatic(NewsReaderFactory.class);
+
+        configurationLoader = mock(ConfigurationLoader.class);
+        newsReader = mock(NewsReader.class);
+    }
+
+    @Test public void oneCallLoadNewsShouldReadOnce() {
+        when(ConfigurationLoader.getInstance()).thenReturn(configurationLoader);
+        when(ConfigurationLoader.getInstance().loadConfiguration()).thenReturn(configuration);
+        when(NewsReaderFactory.getReader(any())).thenReturn(newsReader);
+        when(newsReader.read()).thenReturn(incomingNews);
+        NewsLoader newsLoader = new NewsLoader();
+
+        newsLoader.loadNews();
+
+        verify(newsReader, times(1)).read();
+
+    }
+
+}
